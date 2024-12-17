3255f324bc4a2c5a66a5a1abf64b726763fb0ba0
sameer-bagde <bagdesameer92@gmail.com>
2024-12-17 08:35:13 +0530

    first comment

--- server/index.js
+++ server/index.js
@@ -58,7 +58,15 @@ app.post("/register", async (req, res) => {
 });
 
 
-
+app.get("/users", async (req, res) => {
+    try {
+        const users = await registeredUsers.find(); // Retrieve all users from the database
+        res.json({ users }); // Send the list of users as a response
+    } catch (error) {
+        console.error("Error fetching users:", error);
+        res.status(500).json({ message: "Failed to fetch users. Please try again.", error: error.message });
+    }
+});
 
 
 app.post("/login", async (req, res) => {
